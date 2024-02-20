4afada0ac9756ecc536ab7629c1c6eb3879b7fa9
Pedro Santos <pedrohenriqueph004@gmail.com>
2024-02-20 11:15:24 -0300

    Retirando o Redux para começar do 0

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,6 @@ import Header from "./components/Titulo";
 import EstiloGlobal, { ContainerPagina, ContanainerContato } from "./styles";
 import CardRemover from "./components/CardRemover";
 import { useState } from "react";
-import { Provider } from "react-redux"; 
-import store from "./store";
-
-
 
 function App() {
     const [estaRemovendo, setEstaRemovendo] = useState(false)
@@ -20,28 +16,23 @@ function App() {
     }
 
     return (
-      <Provider store={store}>
-        <EstiloGlobal/>
-        <ContainerPagina>
-            <Header />
-            {estaRemovendo ? (
-              <CardRemover 
-                cancelarRemover={funcaoCancelar}    
-              />
-            ) : (
-              <ContanainerContato>
-                <Contato 
-                    nome='Pedro'
-                    telefone='Pedro'
-                    email='Pedro'
-                    apagar={funcaoApagar}
-                />
-              </ContanainerContato>
-            )}
-            
-            
-        </ContainerPagina>
-      </Provider>
+        <>
+        <EstiloGlobal />
+            <ContainerPagina>
+                  <Header />
+                  {estaRemovendo ? (
+                    <CardRemover
+                      cancelarRemover={funcaoCancelar} />
+                  ) : (
+                    <ContanainerContato>
+                      <Contato
+                        nome='Pedro'
+                        telefone='Pedro'
+                        email='Pedro'
+                        apagar={funcaoApagar} />
+                    </ContanainerContato>
+                  )}
+            </ContainerPagina></>
     ) 
 }
 
